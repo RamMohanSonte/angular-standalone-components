9b160c7d6fe56552cc8eaf3b0c0195d5c645d588
Ram Mohan Sonte <srm.mohan999@gmail.com>
2026-03-15 21:22:08 -0400

    updated

--- routing-example/src/app/products/products.component.ts
+++ routing-example/src/app/products/products.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-products',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss'],
+    selector: 'app-products',
+    imports: [CommonModule],
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {}
